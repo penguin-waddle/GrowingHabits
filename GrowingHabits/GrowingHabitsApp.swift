c93117081f61a1f91c0fa960e8fc4013cd68bd14
Vivien Quach <vivienquach6@gmail.com>
2023-07-18 18:05:45 -0500

    Initial Commit

---
+++ GrowingHabits/GrowingHabitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrowingHabitsApp.swift
+//  GrowingHabits
+//
+//  Created by Vivien on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct GrowingHabitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
